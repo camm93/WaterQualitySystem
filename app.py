7d67aa75f77d6f5104d41011e097a2ec37c5a4d0
camm93 <crismur93@gmail.com>
2024-12-28 12:10:13 -0500

    added example of water sample for the predict/ endpoint

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import joblib
 import pandas as pd
+
+
+class WaterSample(BaseModel):
+    pH: float
+    Dureza: float
+    Sólidos: float
+    Cloraminas: float
+    Sulfatos: float
+    Conductividad: float
+    Carbono_orgánico: float
+    Trihalometanos: float
+    Turbidez: float
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "pH": 7.13,
+                "Dureza": 173.69,
+                "Sólidos": 19309.57,
+                "Cloraminas": 6.53,
+                "Sulfatos": 372.54,
+                "Conductividad": 295.39,
+                "Carbono_orgánico": 7.27,
+                "Trihalometanos": 88.79,
+                "Turbidez": 3.4
+            }
+        }
+
 
 # Create a FastAPI instance
 app = FastAPI()
@@ -8,15 +37,17 @@ app = FastAPI()
 # loaded the trained model
 model = joblib.load("decision_tree_model.pkl")
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Decision Tree Classifier API!"}
 
+
 @app.post("/predict/")
-def predict(data: dict):
+def predict(data: WaterSample):
     try:
         # convert the input data to a DataFrame
-        df = pd.DataFrame.from_dict(data, orient="index").T
+        df = pd.DataFrame.from_dict(data.dict(), orient="index").T
         prediction = model.predict(df)
         prediction_proba = model.predict_proba(df)
         return {
